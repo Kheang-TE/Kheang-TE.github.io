2934ecd27a1be38d3f1ee4cabbcb30e9bf3f7f9c
Kheang-TE <kheang.te07@gmail.com>
2024-11-30 21:05:20 +0100

    style: section projects

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,6 +1,6 @@
 function Contact() {
     return (
-        <section id="contact">
+        <section id="contact" className="backgroundColor">
             <div className="title-container">
                 <h1 className="title-content">Me contacter</h1>
             </div>
